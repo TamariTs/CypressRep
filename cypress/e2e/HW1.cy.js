d615677e299a5a4eea16c2330bf703751733c780
TamariTs <takotsikara@gmail.com>
2025-04-08 13:59:35 +0400

    save changes1

--- cypress/e2e/HW1.cy.js
+++ cypress/e2e/HW1.cy.js
@@ -3,11 +3,8 @@ describe('template spec', () => {
     cy.visit('https://automationexercise.com/')
     cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
     cy.get('.signup-form > h2').should("have.text", 'New User Signup!')
-    //ან
-    cy.contains('New User Signup!').should("be.visible")
     cy.get('[data-qa="signup-name"]').type("Tamari1")
-    const randomNumber= Math.round(Math.random()*10000)
-    cy.get('[data-qa="signup-email"]').type('takotsikara+'+randomNumber+'@gmail.com')
+    cy.get('[data-qa="signup-email"]').type('takotsikara@gmail.com')
     cy.get('[data-qa="signup-button"]').click()
     cy.get(':nth-child(1) > b').should("have.text",'Enter Account Information')
     cy.get('#id_gender1').click()
