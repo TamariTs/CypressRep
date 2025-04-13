808c7e875e8fa32439c97f3b720bd514f2ff4d36
TamariTs <takotsikara@gmail.com>
2025-04-13 19:20:53 +0400

    save the very last changes

--- cypress/e2e/NHW3.cy.js
+++ cypress/e2e/NHW3.cy.js
@@ -1,24 +1,14 @@
 
 describe('template spec', () => {
-  before(()=>{
-    //cy.fixture("newfile.json").as("Userdata")
+  before(() => {
+  
     cy.fixture("IncorrectLogin.json").as("Incorrect")
   })
-  it('passes', function (){
+  it('passes', function () {
     cy.visit('http://automationexercise.com')
     cy.Login2(this.Incorrect.email, this.Incorrect.password)
-
-    //4. Click on 'Signup / Login' button
-    //cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
-    // 5. Verify 'Login to your account' is visible
     cy.get('.login-form > h2').should("be.visible")
     cy.get('.login-form > h2').should("have.text", 'Login to your account')
-    // 6. Enter incorrect email address and password
-    //cy.get('[data-qa="login-email"]').type("takotsikara@gmail.com")
-    //cy.get('[data-qa="login-password"]').type("Wikara113")
-    // 7. Click 'login' button
-    //cy.get('[data-qa="login-button"]').click()
-    // 8. Verify error 'Your email or password is incorrect!' is visible
     cy.get('.login-form > form > p').should("have.text", 'Your email or password is incorrect!')
   })
 })
